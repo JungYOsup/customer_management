ab80eaae2f01e671a86d2433e2bdff56a53f69a3
yosup <yosup1004@gmail.com>
2020-07-10 12:41:00 +0900

    add material ul

--- customer_management/src/App.js
+++ customer_management/src/App.js
@@ -1,5 +1,23 @@
 import React, { Component } from "react";
 import Customer from "./components/Customer";
+import Paper from "@material-ui/core/Paper";
+import Table from "@material-ui/core/Table";
+import TableHead from "@material-ui/core/TableHead";
+import TableBody from "@material-ui/core/TableBody";
+import TableRow from "@material-ui/core/TableRow";
+import TableCell from "@material-ui/core/TableCell";
+import { withStyles } from "@material-ui/core/styles";
+
+const styles = (theme) => ({
+  root: {
+    width: "100%",
+    marginTop: theme.spacing.unit * 3,
+    overflowX: "auto",
+  },
+  table: {
+    minWidth: 1080,
+  },
+});
 
 const customers = [
   {
@@ -30,22 +48,35 @@ const customers = [
 
 class App extends Component {
   render() {
+    const { classes } = this.props;
     return (
-      <div>
-        {customers.map((custom) => (
-          <Customer
-            key={custom.id}
-            id={custom.id}
-            name={custom.name}
-            image={custom.image}
-            birthday={custom.birthday}
-            gender={custom.gender}
-            job={custom.job}
-          />
-        ))}
-      </div>
+      <Paper className={classes.root}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableCell>번호</TableCell>
+            <TableCell>이미지</TableCell>
+            <TableCell>이름</TableCell>
+            <TableCell>생년월일</TableCell>
+            <TableCell>성별</TableCell>
+            <TableCell>직업</TableCell>
+          </TableHead>
+          <TableBody>
+            {customers.map((custom) => (
+              <Customer
+                key={custom.id}
+                id={custom.id}
+                name={custom.name}
+                image={custom.image}
+                birthday={custom.birthday}
+                gender={custom.gender}
+                job={custom.job}
+              />
+            ))}
+          </TableBody>
+        </Table>
+      </Paper>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
